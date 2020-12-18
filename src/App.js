910a91d9ad683e91984b61615b29ae55ced5247d
Mario Menezes <mario082@gmail.com>
2020-12-17 22:55:58 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           </div>
           <div>
               <a href="/cart">Cart</a>
-              <a href="/signin">Sign In tste</a>
+              <a href="/signin">Sign In</a>
           </div>
       </header>
       <main>
