09b9fa131553264d90b4f4af8dc9e573e6e4fff7
Mario Menezes <mario082@gmail.com>
2020-12-21 16:32:30 -0300

    página produto

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Rating from './Rating';
 
 export default function Product(props) {
@@ -6,11 +6,11 @@ export default function Product(props) {
     return (
        
             <div key={product._id} className="card">
-                <a href="{`/product/${product._id}`}">
+                <a href={`/product/${product._id}`}>
                     <img className="medium" src={product.image} alt="{product.name"/>
                 </a>
                 <div className="card-body">
-                    <a href="{`/product/${product._id}`}">
+                    <a href={`/product/${product._id}`}>
                         <h2>{product.name}</h2>
                     </a>
                     <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
